3476529188f6b0d6452e1914bbcc1ccbec68aebe
dlsgk2016 <86703264+flyforme2016@users.noreply.github.com>
2021-10-22 17:04:50 +0900

    file rename

--- app/src/main/java/com/example/muf/Community_frag.java
+++ app/src/main/java/com/example/muf/Community_frag.java
@@ -1,6 +1,8 @@
 package com.example.muf;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,16 +10,33 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class Community_frag extends Fragment {
 
     private View view;
+    static final String TAG = "HOME";
+
+    public static Community_frag newinstance(){
+        return new Community_frag();
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.community_layout, container, false);
 
+        view.findViewById(R.id.floatingActionButton).setOnClickListener(onClickListener);
         return view;
     }
+
+    View.OnClickListener onClickListener = (v) -> {
+        switch (v.getId()){
+            case R.id.floatingActionButton:
+                Intent intent = new Intent(getActivity(), AddPostActivity.class);
+                startActivity(intent);
+                break;
+        }
+    };
 }
--- app/src/main/java/com/example/muf/homeActivity.java
+++ app/src/main/java/com/example/muf/homeActivity.java
@@ -2,6 +2,7 @@ package com.example.muf;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -12,12 +13,12 @@ import android.view.MenuItem;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class homeActivity extends AppCompatActivity {
-        static final String TAG = "HOME";
-        private BottomNavigationView bottomNavigationView;
-        private FragmentManager fm;
-        private FragmentTransaction ft;
-        private Friends_list_frag frag1;
-        private Chatting_frag frag2;
+    static final String TAG = "HOME";
+    private BottomNavigationView bottomNavigationView;
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+    private Friends_list_frag frag1;
+    private Chatting_frag frag2;
     private Home_frag frag3;
     private Community_frag frag4;
     private Myprofile_frag frag5;
@@ -57,9 +58,12 @@ public class homeActivity extends AppCompatActivity {
         frag3 = new Home_frag();
         frag4 = new Community_frag();
         frag5 = new Myprofile_frag();
+
         setFrag(2); //첫 프래그먼트 화면 지정
 
     }
+
+
 
     //프래그먼트 교체가 일어나는 실행문
     private void setFrag(int n){
@@ -94,4 +98,5 @@ public class homeActivity extends AppCompatActivity {
 
         }
     }
+
 }
